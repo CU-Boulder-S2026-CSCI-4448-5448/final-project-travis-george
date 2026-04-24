847f20239308f366f84c2315f92887daf5772bc2
Travis Uhrig <travisformayor@users.noreply.github.com>
2026-04-24 14:11:18 -0600

    move render from field to view

--- src/main/java/weathersim/Simulation.java
+++ src/main/java/weathersim/Simulation.java
@@ -1,18 +1,15 @@
 package weathersim;
 
 import weathersim.fields.MoistureField;
-import weathersim.fields.ResultField;
 import weathersim.fields.TempField;
 
 public class Simulation {
     private final TempField tempField;
     private final MoistureField moistureField;
-    private final ResultField resultField;
 
     public Simulation(int rows, int cols) {
         this.tempField = new TempField(rows, cols);
         this.moistureField = new MoistureField(rows, cols, tempField);
-        this.resultField = new ResultField(moistureField);
     }
 
     public void update() {
@@ -26,9 +23,5 @@ public class Simulation {
 
     public MoistureField getMoistureField() {
         return this.moistureField;
-    }
-
-    public ResultField getResultField() {
-        return this.resultField;
     }
 }
--- src/main/java/weathersim/views/ResultViewStrategy.java
+++ src/main/java/weathersim/views/ResultViewStrategy.java
@@ -5,11 +5,12 @@ import processing.core.PGraphics;
 import weathersim.Simulation;
 
 public class ResultViewStrategy implements IViewStrategy{
+    public static final float CLOUD_THRESHOLD = 0.25f;
 
     @Override
     public void render(PGraphics g, Simulation sim) {
         drawSky(g);
-        sim.getResultField().renderClouds(g);
+        drawClouds(g, sim);
     }
 
     private void drawSky(PGraphics g) {
@@ -18,4 +19,22 @@ public class ResultViewStrategy implements IViewStrategy{
         g.rect(0, 0, g.width, g.height);
     }
 
+    private void drawClouds(PGraphics g, Simulation sim) {
+        int rows = sim.getMoistureField().getNumRows();
+        int cols = sim.getMoistureField().getNumCols();
+        float cellWidth = (float) g.width / cols;
+        float cellHeight = (float) g.height / rows;
+        g.rectMode(PConstants.CORNER);
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                float moisture = sim.getMoistureField().getCell(row, col);
+                if (moisture >= CLOUD_THRESHOLD) {
+                    float x = (col) * cellWidth;
+                    float y = (row) * cellHeight;
+                    g.fill(255);
+                    g.rect(x, y, cellWidth, cellHeight);
+                }
+            }
+        }
+    }
 }
